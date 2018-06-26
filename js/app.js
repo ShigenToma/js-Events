9ee98785ccd193ae485c72413dd50180f86ef1a3
unknown <shigntoma@gmail.com>
2018-06-25 19:46:57 -1000

    events

--- js/app.js
+++ js/app.js
@@ -6,35 +6,88 @@ The function will add the following content inside the paragraph with the id of 
 
 var bacon = "Shoulder turducken brisket, kevin swine andouille tri-tip salami tail ham sausage pork loin. Ribeye short loin rump kielbasa pork. Capicola short loin turducken corned beef tongue, chuck leberkas salami frankfurter. Kielbasa fatback pancetta, ground round meatball turducken jowl ribeye alcatra sirloin bacon corned beef beef ribs short loin. Pork belly spare ribs biltong corned beef meatball short ribs tongue alcatra swine drumstick. Biltong shankle kevin, cupim sirloin bresaola brisket. Tail pork belly biltong ball tip tri-tip, pig jerky cow pastrami prosciutto ;ground round bacon capicola tongue meatball.";
 
+document.getElementById("one").addEventListener("onclick",moreContent);
+function moreContent() {
+ 
+    document.getElementById("more").innerHTML =(bacon);
+}
 
 //2. HTTP
 /*Create a function named `lessContent` that will initiate the `Show Less` link after clicking on it.
 
 The function will hide the contents in the pargraph with the id of `less` after clicking on the `Show Less` link.*/
-
+document.getElementById("noMore").addEventListener("onclick",lessContent);
+function lessContent() {
+ 
+    document.getElementById("less").innerHTML =('');
+}
 
 //3. Tacocat, The Original Palindrome King
 /*Create a function named `zoom` that will increase the font size of the paragraph with the id of `biggie` after hovering your mouse over it. Increae the font size to 150%*/
 
+// function zoom() {
+//     document.getElementById("biggie").style.fontSize  = "150%";
+//     }
+
+// function zoom(e) {
+//     if (e.type=='mouseover') {
+//           this.style.fontSize = "150%";   
+//     } else {
+
+//           this.style.fontSize = "100%";  
+//     }
+// }
+
+// var elm = document.getElementById('biggie')
+//  elm.onmouseout =zoom;
+//  elm.onmouseover=zoom;
+
+ function zoom() {
+ document.getElementById("biggie").style.fontSize  = "150%";
+ }
 
 //4. McDonalds
 /*Create a variable name menuItems and assign it an array of three of your favorite items at McDonald's.
 
-Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
 
+Next, create a function named valueMenu that will display your favorite items in the paragraph the the id of `menu` after clicking on the showMenu paragraph.*/
+var menuItems = ['French Fries', 'McDouble', 'Coke'];
+ document.getElementById("showMenu").addEventListener("onclick",valueMenu);
+function valueMenu() {
+ 
+    document.getElementById("menu").innerHTML =(menuItems);
+}
 
 //5. Gin.
 /*Create a function named redFace that will change the paragraph text to red and a font size of 30px after clicking on the text.*/
 
+function redFace() {
+    document.getElementById("drink").style.fontSize = "30px";  
+    document.getElementById("drink").style.color = "red";
+  }
+  
+
 
 //6. Peanut Butter Cup Oreos
 /*Create a function `showPrice` that will add the price of `$5.55` inside the paragraph with the id `price` after hovering your mouse over the paragraph.*/
+document.getElementById("oreo").addEventListener("onmouseover",showPrice);
 
-
+var oreoPrice = 5.55;
+function showPrice() {
+ 
+    document.getElementById("price").innerHTML =(oreoPrice);
+}
 //7. Mr. Buttons
 /*Add an Event Listener to the button that will display `myQuote` inside the paragraph with the id of `displayQuote` after the button is clicked.*/
 
-var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
+
+ var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
+document.getElementById("Benjamin").addEventListener("click", quote);
+
+function quote() {
+ 
+   document.getElementById("displayQuote").innerHTML =(myQuote);
+}
 
 
 //8. Say It again, Randomly
@@ -42,8 +95,22 @@ var myQuote = "Our lives are defined by opportunities; even the ones we miss.";
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
-
+document.getElementById("random").addEventListener("click", ranQuotes);
+function ranQuotes() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('displayQuotes').innerHTML = quotes[randomNumber];
+}
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
 */
+document.getElementById("showHide").addEventListener("click", cat);
+function cat() {
+ // console.log('test')
+    // var x = document.getElementById("catmoney");
+    if (showmoney.style.display === "none") {
+        showmoney.style.display = "block";
+    } else {
+        showmoney.style.display = "none";
+    }
+}
